f18dcaab614f325dd76cebfe69b974def4bef5f1
Chris Mangum <peril10@hotmail.com>
2023-10-26 18:01:07 -0700

    updates

--- game/graph.py
+++ game/graph.py
@@ -1,36 +1,56 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# The given string representation
+maze_string = """
+X X X X X X X X X X ...
+... (rest of your string here)
+X X X X X X X X X X X X X X X X X X X X X X X X X X X X
+"""
+
+# Convert the string representation to a 2D list
+maze_2d = [list(row.split()) for row in maze_string.strip().split("\n")]
+
+# Create a grid graph
+G = nx.grid_2d_graph(len(maze_2d), len(maze_2d[0]))
+
+for i in range(len(maze_2d)):
+    for j in range(len(maze_2d[0])):
+        G.nodes[(i, j)]['label']  = maze_2d[i][j]
+
+# Draw the graph
+pos = dict((n, n) for n in G.nodes())
+labels = nx.get_node_attributes(G, 'label')
+nx.draw(G, pos, with_labels=True, labels=labels)
+plt.show()
+
+
 
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def create_grid_graph(L, W):
-    # Create an empty graph
-    G = nx.Graph()
+def create_grid_graph(dim):
+    G = nx.grid_2d_graph(dim, dim)
+    
+    # Wrap around the edges
+    for x in range(dim):
+        G.add_edge((x, 0), (x, dim-1))
+        G.add_edge((0, x), (dim-1, x))
 
-    # Add nodes to the graph
-    for i in range(L):
-        for j in range(W):
-            G.add_node((i, j))
-
-    # Add edges to the graph
-    for i in range(L):
-        for j in range(W):
-            # Connect to the node to the right
-            if i < L - 1:
-                G.add_edge((i, j), (i + 1, j))
-            # Connect to the node below
-            if j < W - 1:
-                G.add_edge((i, j), (i, j + 1))
+    # Label nodes with integers instead of (x, y) coordinates
+    relabel_dict = {(x, y): y*dim + x for x, y in G.nodes()}
+    G = nx.relabel_nodes(G, relabel_dict)
 
     return G
 
-# Example usage:
-L, W = 5, 5
-G = create_grid_graph(L, W)
+def coordinate_to_node_label(x, y, dim):
+    return y * dim + x
 
-# one hop from 2,2
-print(G[(4,4)])
+# Create a 10x10 grid graph
+G = create_grid_graph(10)
 
-# Draw the graph
-pos = {(x, y): (y, -x) for x, y in G.nodes()}
-nx.draw(G, pos=pos, with_labels=True, node_size=100, node_color="blue")
+# Draw the graph with updated position mapping
+dim = 10
+pos = {(x + y*10): (x, dim - 1 - y) for x in range(dim) for y in range(dim)}
+nx.draw(G, pos, with_labels=True, node_size=400, node_color="lightblue")
 plt.show()
